adbf661e05bddd57119275aeb1ae77cd2d62efce
Madison <Madison@LAPTOP-2AIPK137>
2024-10-16 00:59:17 -0400

    Add project files.

---
+++ Assignment_3/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Assignment_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "p51.png");
+            this.imageList1.Images.SetKeyName(1, "p50.png");
+            this.imageList1.Images.SetKeyName(2, "p49.png");
+            this.imageList1.Images.SetKeyName(3, "p48.png");
+            this.imageList1.Images.SetKeyName(4, "p47.png");
+            this.imageList1.Images.SetKeyName(5, "p46.png");
+            this.imageList1.Images.SetKeyName(6, "p45.png");
+            this.imageList1.Images.SetKeyName(7, "p44.png");
+            this.imageList1.Images.SetKeyName(8, "p43.png");
+            this.imageList1.Images.SetKeyName(9, "p42.png");
+            this.imageList1.Images.SetKeyName(10, "p41.png");
+            this.imageList1.Images.SetKeyName(11, "p40.png");
+            this.imageList1.Images.SetKeyName(12, "p39.png");
+            this.imageList1.Images.SetKeyName(13, "p38.png");
+            this.imageList1.Images.SetKeyName(14, "p37.png");
+            this.imageList1.Images.SetKeyName(15, "p36.png");
+            this.imageList1.Images.SetKeyName(16, "p35.png");
+            this.imageList1.Images.SetKeyName(17, "p34.png");
+            this.imageList1.Images.SetKeyName(18, "p33.png");
+            this.imageList1.Images.SetKeyName(19, "p32.png");
+            this.imageList1.Images.SetKeyName(20, "p31.png");
+            this.imageList1.Images.SetKeyName(21, "p30.png");
+            this.imageList1.Images.SetKeyName(22, "p29.png");
+            this.imageList1.Images.SetKeyName(23, "p28.png");
+            this.imageList1.Images.SetKeyName(24, "p27.png");
+            this.imageList1.Images.SetKeyName(25, "p26.png");
+            this.imageList1.Images.SetKeyName(26, "p25.png");
+            this.imageList1.Images.SetKeyName(27, "p24.png");
+            this.imageList1.Images.SetKeyName(28, "p23.png");
+            this.imageList1.Images.SetKeyName(29, "p22.png");
+            this.imageList1.Images.SetKeyName(30, "p21.png");
+            this.imageList1.Images.SetKeyName(31, "p20.png");
+            this.imageList1.Images.SetKeyName(32, "p19.png");
+            this.imageList1.Images.SetKeyName(33, "p18.png");
+            this.imageList1.Images.SetKeyName(34, "p17.png");
+            this.imageList1.Images.SetKeyName(35, "p16.png");
+            this.imageList1.Images.SetKeyName(36, "p15.png");
+            this.imageList1.Images.SetKeyName(37, "p14.png");
+            this.imageList1.Images.SetKeyName(38, "p13.png");
+            this.imageList1.Images.SetKeyName(39, "p12.png");
+            this.imageList1.Images.SetKeyName(40, "p11.png");
+            this.imageList1.Images.SetKeyName(41, "p10.png");
+            this.imageList1.Images.SetKeyName(42, "p9.png");
+            this.imageList1.Images.SetKeyName(43, "p8.png");
+            this.imageList1.Images.SetKeyName(44, "p7.png");
+            this.imageList1.Images.SetKeyName(45, "p6.png");
+            this.imageList1.Images.SetKeyName(46, "p5.png");
+            this.imageList1.Images.SetKeyName(47, "p4.png");
+            this.imageList1.Images.SetKeyName(48, "p3.png");
+            this.imageList1.Images.SetKeyName(49, "p2.png");
+            this.imageList1.Images.SetKeyName(50, "p1.png");
+            this.imageList1.Images.SetKeyName(51, "p0.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Poker Hand ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Assignment_3/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Assignment_3
+{
+    public partial class Form1 : Form
+    {
+        private const int Handsize = 5;
+        private const string Path = "C:\\Users\\Madison\\Downloads\\";
+        private readonly List<int> deckList = new List<int>();
+        private readonly int[] handArray = new int[Handsize];
+        private readonly ImageList imageList = new ImageList();
+
+        private readonly PictureBox[] pictureBoxes = new PictureBox[Handsize];
+        private readonly CheckBox[] checkBoxes = new CheckBox[Handsize];
+
+        private Button dealButton, saveButton, loadButton;
+        public Form1()
+        {
+            this.Text = "Card Game";
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            InitializeComponent();
+
+            InitializeControls();
+
+            InitializeImageList();
+
+            InitializeDeck();
+
+            DealHand();
+        }
+
+        private void InitializeImageList()
+        {
+            imageList.ImageSize = new Size(100, 150);
+
+            for (int i = 0; i < 52; i++)
+            {
+                string filePath = Path + $"p{i}.png";
+                imageList.Images.Add(Image.FromFile(filePath));
+            }
+
+        }
+
+        private void InitializeControls()
+        {
+            dealButton = new Button { Text = "&Deal Hand", Location = new Point(639, 71) };
+            dealButton.Click += (s, e) => DealHand();
+
+            saveButton = new Button { Text = "&Save Hand", Location = new Point(484, 71) };
+            saveButton.Click += (s, e) => SaveHand();
+
+            loadButton = new Button { Text = "&Load Hand", Location = new Point(40, 71) };
+            loadButton.Click += (s, e) => LoadHand();
+
+            this.Controls.Add(dealButton);
+            this.Controls.Add(saveButton);
+            this.Controls.Add(loadButton);
+
+
+
+            for (int i = 0; i < Handsize; i++)
+            {
+                checkBoxes[i] = new CheckBox
+                {
+
+                    Text = $"&Keep {i +1}",
+                    Location = new Point(40 + i * 160, 128),
+                    TabIndex = i + 1,
+                    Checked = i >= 0
+                };
+
+                int index = i;
+                checkBoxes[i].CheckedChanged += (s, e) => ToggleKeep(index);
+  
+                this.Controls.Add(checkBoxes[i]);
+
+                pictureBoxes[i] = new PictureBox
+                {
+                    Location = new Point(40 + i * 160, 168),
+                    Size = new System.Drawing.Size(100, 150),
+                    Image = null,
+                    TabStop = false,
+                    BorderStyle = BorderStyle.FixedSingle
+
+                };
+                this.Controls.Add(pictureBoxes[i]);     
+            }
+        }
+
+        private void InitializeDeck()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                deckList.Add(i);
+            }
+            Shuffle(deckList);
+        }
+
+             private readonly OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "Card files (*.card)|*.card|All files (*.*)|*.*",
+                    FilterIndex = 1,
+                    RestoreDirectory = true
+                };
+
+               private readonly SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "Card files (*.card)|*.card|All files (*.*)|*.*",
+                    FilterIndex = 1,
+                    RestoreDirectory = true
+                };
+
+     private void DealHand()
+{
+            for (int i = 0; i < Handsize; i++)
+            {
+                if (!checkBoxes[i].Checked)
+                {
+                    if (deckList.Count > 0)
+                    {
+                        handArray[i] = deckList[0];
+                        deckList.RemoveAt(0);
+                    }
+                    else
+                    {
+                        handArray[i] = -1;
+                    }
+                }
+                    UpdateCardDisplay(i);
+              
+            }
+}
+
+            private void ToggleKeep(int index)
+            {
+               bool isChecked = checkBoxes[index].Checked;
+            if(isChecked)   
+            {
+                handArray[index] = deckList.Count > 0 ? deckList[0] : -1;
+            }
+            else
+            {
+                handArray[index] = -1;
+            }
+                UpdateCardDisplay(index);
+            }
+
+            private void UpdateHand()
+            {
+                for (int i = 0; i < Handsize; i++)
+                {
+                    if (checkBoxes[i].Checked)
+                    {
+                        handArray[i] = handArray[i] >= 0 ? handArray[i] : -1;
+                    }
+                    else
+                    {
+                        handArray[i] = -1;
+                    }
+                    UpdateCardDisplay(i);
+                }
+
+            }
+
+            private void UpdateCardDisplay(int index)
+            {
+                pictureBoxes[index].Image = handArray[index] >= 0 ? imageList.Images[handArray[index]] : null;
+            }
+
+            private void Shuffle(List<int> list)
+            {
+                Random random = new Random();
+                int n = list.Count;
+                while (n > 1)
+                {
+                    n--;
+                    int k = random.Next(n + 1);
+                (list[n], list[k]) = (list[k], list[n]);
+            }
+        }
+
+        private void SaveHand()
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog.FileName))
+                    {
+                        foreach (int card in handArray)
+                        {
+                            file.WriteLine(card);
+                        }
+                    }
+                }
+            }
+
+            private void LoadHand()
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog.FileName))
+                    {
+                        for (int i = 0; i < Handsize; i++)
+                        {
+                            handArray[i] = int.Parse(file.ReadLine());
+                            UpdateCardDisplay(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
